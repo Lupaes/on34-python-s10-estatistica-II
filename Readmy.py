21e209bbd2fa6b41696bc7606cb03a6db11b2ece
Lupaes <luanapaes@yahoo.com.br>
2024-08-09 19:48:15 -0300

    Create Readmy.py

---
+++ Readmy.py
@@ -0,0 +1,47 @@
+#Objetivo 🌍
+
+🛠 Realizar Testes de Hipótese
+
+• ⛔Utilizando:
+• ⛔⛔Teste t de Student:
+• Biblioteca: scipy.stats
+• Função: ttest_ind(): Para comparar as médias de duas amostras independentes.
+• Função: ttest_rel(): Para comparar as médias de duas amostras dependentes (pareadas).
+• Função: ttest_1samp(): Para comparar a média de uma amostra com um valor de referência.
+
+• ⛔⛔Teste Z de Proporções:
+• Biblioteca: statsmodels.stats.proportion
+• Função: proportions_ztest(): Para testar uma proporção ou comparar proporções de duas amostras.
+
+• ⛔⛔Teste Qui-Quadrado:
+• Biblioteca: scipy.stats
+• Função: chi2_contingency(): Para testar a independência entre duas variáveis categóricas.
+
+• ⛔⛔Teste ANOVA (Análise de Variância):
+• Biblioteca: scipy.stats
+• Função: f_oneway(): Para comparar as médias de três ou mais grupos independentes.
+
+#Recursos 👩🏽‍💻
+
+•	🎓 Professora: Aula de revisão 
+•	🛠 Classrrom: Repositórios de todas as aulas 
+•	💻 Videos das aulas: Youtube 
+•	👩🏽‍🤝‍👩🏻 Grupo do Whatsapp: Nenhuma solta a mão da outra 
+•	🤖 Colab:https://colab.research.google.com/ 
+
+#Desenvolvimento do código
+
+##Estratégia escolhida🎯
+• Identificar qual o tipo de de amostra realcionada a cada exercício.
+• Aplicação das Bibliotecas
+• Utilização da plataforma do Colab 
+•
+#Comentários finais 💪🏽 
+  
+• Achei bem desafiador esse exercício.🔥 
+• O resultado foi muito legal, foi muito emocionante ver o código rodar, porém próximo do que eu almejava, visto que ao rodar o código a resposta se repetia.
+•
+• O código foi todo desenvolvido pela plataforma do Colab e seu link se encontra na pasta Código Colab comentários.
+•  
+• Adorei ❤️ 
+• Feito com ❤️ por Luana Paes
